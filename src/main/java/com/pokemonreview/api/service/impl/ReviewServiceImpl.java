47406c3e2789c4390a9498be2e9438e25b413916
최경섭 <kyoungschoi@gmail.com>
2023-07-24 15:30:25 +0900

    service

--- src/main/java/com/pokemonreview/api/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/pokemonreview/api/service/impl/ReviewServiceImpl.java
@@ -1,5 +1,6 @@
 package com.pokemonreview.api.service.impl;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.pokemonreview.api.dto.ReviewDto;
 import com.pokemonreview.api.exceptions.ResourceNotFoundException;
 import com.pokemonreview.api.models.Pokemon;
@@ -7,6 +8,7 @@ import com.pokemonreview.api.models.Review;
 import com.pokemonreview.api.repository.PokemonRepository;
 import com.pokemonreview.api.repository.ReviewRepository;
 import com.pokemonreview.api.service.ReviewService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -58,8 +60,8 @@ public class ReviewServiceImpl implements ReviewService {
 
 
     @Override
-    public List<ReviewDto> getReviewsByPokemonId(int id) {
-        List<Review> reviews = reviewRepository.findByPokemonId(id);
+    public List<ReviewDto> getReviewsByPokemonId(int pokemonId) {
+        List<Review> reviews = reviewRepository.findByPokemonId(pokemonId);
 
         return reviews.stream()
                 .map(review -> mapToDto(review))
@@ -74,7 +76,7 @@ public class ReviewServiceImpl implements ReviewService {
 
         if(review.getPokemon().getId() != pokemon.getId()) {
             throw new ResourceNotFoundException(
-                    "This review does not belond to a pokemon");
+                    "This review does not belong to a pokemon");
         }
 
         return mapToDto(review);
@@ -97,9 +99,9 @@ public class ReviewServiceImpl implements ReviewService {
         review.setContent(reviewDto.getContent());
         review.setStars(reviewDto.getStars());
 
-        Review updateReview = reviewRepository.save(review);
+        //Review updateReview = reviewRepository.save(review);
 
-        return mapToDto(updateReview);
+        return mapToDto(review);
     }
 
     @Override
@@ -118,10 +120,11 @@ public class ReviewServiceImpl implements ReviewService {
 
     private ReviewDto mapToDto(Review review) {
         ReviewDto reviewDto = new ReviewDto();
-        reviewDto.setId(review.getId());
-        reviewDto.setTitle(review.getTitle());
-        reviewDto.setContent(review.getContent());
-        reviewDto.setStars(review.getStars());
+        BeanUtils.copyProperties(review, reviewDto);
+//        reviewDto.setId(review.getId());
+//        reviewDto.setTitle(review.getTitle());
+//        reviewDto.setContent(review.getContent());
+//        reviewDto.setStars(review.getStars());
         return reviewDto;
     }
 
