c923acc59ae116865861d70bd8d306adab4d0fe3
최경섭 <kyoungschoi@gmail.com>
2023-07-24 13:37:52 +0900

    test

--- src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
+++ src/test/java/com/pokemonreview/api/repository/PokemonRepositoryTests.java
@@ -9,11 +9,12 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.Optional;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
-class PokemonRepositoryTests {
+public class PokemonRepositoryTests {
 
     @Autowired
     private PokemonRepository pokemonRepository;
@@ -25,7 +26,6 @@ class PokemonRepositoryTests {
                 .name("pikachu")
                 .type(PokemonType.ELECTRIC)
                 .build();
-
         //Act
         Pokemon savedPokemon = pokemonRepository.save(pokemon);
 
@@ -33,5 +33,90 @@ class PokemonRepositoryTests {
         Assertions.assertThat(savedPokemon).isNotNull();
         Assertions.assertThat(savedPokemon.getId())
                 .isGreaterThan(0);
+    }
+
+    @Test
+    public void PokemonRepository_GetAll_ReturnMoreThenOnePokemon() {
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type(PokemonType.ELECTRIC).build();
+        Pokemon pokemon2 = Pokemon.builder()
+                .name("Raichu")
+                .type(PokemonType.NORMAL).build();
+
+        pokemonRepository.save(pokemon);
+        pokemonRepository.save(pokemon2);
+
+        List<Pokemon> pokemonList = pokemonRepository.findAll();
+
+        Assertions.assertThat(pokemonList).isNotNull();
+        Assertions.assertThat(pokemonList.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void PokemonRepository_FindById_ReturnPokemon() {
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type(PokemonType.ELECTRIC).build();
+
+        pokemonRepository.save(pokemon);
+
+        Pokemon savedPokemon = pokemonRepository
+                .findById(pokemon.getId())
+                .get();
+
+        Assertions.assertThat(savedPokemon).isNotNull();
+        Assertions.assertThat(savedPokemon.getName()).isEqualTo("pikachu");
+    }
+
+    @Test
+    public void PokemonRepository_FindByType_ReturnPokemonNotNull() {
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type(PokemonType.ELECTRIC).build();
+
+        pokemonRepository.save(pokemon);
+
+        Pokemon pokemonList = pokemonRepository
+                .findByType(pokemon.getType())
+                .get();
+
+        Assertions.assertThat(pokemonList).isNotNull();
+        Assertions.assertThat(pokemonList.getType().name()).isEqualTo(PokemonType.ELECTRIC.name());
+        System.out.println("PokemonType.ELECTRIC.name()"+PokemonType.ELECTRIC.name());
+    }
+
+    @Test
+    public void PokemonRepository_UpdatePokemon_ReturnPokemonNotNull() {
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type(PokemonType.ELECTRIC)
+                .build();
+
+        pokemonRepository.save(pokemon);
+
+        Pokemon pokemonSave = pokemonRepository.findById(pokemon.getId()).get();
+        pokemonSave.setName("Raichu");
+        pokemonSave.setType(PokemonType.NORMAL);
+
+        Assertions.assertThat(pokemonSave.getName()).isEqualTo("Raichu");
+        Assertions.assertThat(pokemonSave.getType()).isEqualTo(PokemonType.NORMAL);
+
+        System.out.println("pokemonSave = " + pokemonSave);
+    }
+
+    @Test
+    public void PokemonRepository_PokemonDelete_ReturnPokemonIsEmpty() {
+        Pokemon pokemon = Pokemon.builder()
+                .name("pikachu")
+                .type(PokemonType.ELECTRIC)
+                .build();
+
+        pokemonRepository.save(pokemon);
+
+        pokemonRepository.deleteById(pokemon.getId());
+        Optional<Pokemon> pokemonReturn = pokemonRepository.findById(pokemon.getId());
+
+        Assertions.assertThat(pokemonReturn).isEmpty();
     }
 }
